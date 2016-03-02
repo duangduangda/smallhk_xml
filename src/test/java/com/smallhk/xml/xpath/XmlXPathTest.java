972934b2f7c885874ddf0de16193066e8343a3fa
duangduangda <dongyhemail@gmail.com>
2016-03-02 08:59:28 +0800

    update

--- src/test/java/com/smallhk/xml/xpath/XmlXPathTest.java
+++ src/test/java/com/smallhk/xml/xpath/XmlXPathTest.java
@@ -1,7 +1,6 @@
 package com.smallhk.xml.xpath;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.IOException;
 
